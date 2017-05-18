1d266517174ac2efcc88322507e1aae09d599f84
nhnaoto <nhnaoto.nh@gmail.com>
2017-05-18 14:54:53 +0800

    add seed logic

--- Gemfile
+++ Gemfile
@@ -41,13 +41,13 @@ gem 'jquery-rails'
 gem 'rails_admin', '~> 1.1.1'
 
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,84 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+EventUser.delete_all
+Category.delete_all
+Event.delete_all
+User.delete_all
+UserCategory.delete_all
+
+category = {}
+ActiveRecord::Base.transaction do
+    category['name'] = 'foods'
+    category['group'] = 0
+    Category.create(category)
+    category['name'] = 'sports'
+    category['group'] = 0
+    Category.create(category)
+    category['name'] = 'entertaiments'
+    category['group'] = 0
+    Category.create(category)
+    category['name'] = 'category4'
+    category['group'] = 0
+    Category.create(category)
+    category['name'] = 'category5'
+    category['group'] = 0
+    Category.create(category)
+    category['name'] = 'category6'
+    category['group'] = 0
+    Category.create(category)
+end
+
+
+user = {}
+10.times do 
+    user['fullname'] = Faker::Name.name 
+    user['email'] = Faker::Internet.email
+    user['password'] = 'test'
+    User.create(user)
+end
+
+uids = []
+User.all.each { |u| uids << u.id }
+cids = []
+Category.all.each { |c| cids << c.id }
+
+event = {}
+ActiveRecord::Base.transaction do
+	20.times do 
+		cid = cids.sample
+	    event['name'] = Faker::Food.ingredient
+	    event['name'] = Faker::Vehicle.manufacture if cid ==2
+	    event['name'] = Faker::Music.instrument if cid ==3
+
+	    event['venue'] = Faker::Address.street_name
+	    event['description'] = 'TESTESTSETSETSETESTSETSETSTETESTSETESTSETSETSETESTESTESESTSETSETESTES'
+	    event['event_time'] = Faker::Date.between(100.days.ago, Date.today)
+	    event['user_id'] = uids.sample
+	  	event['category_id'] = cid
+	    Event.create(event)
+	end
+end
+
+eids = []
+Event.all.each { |e| eids << e.id }
+
+user_category = {}
+ActiveRecord::Base.transaction do
+	20.times do 
+	    user_category['user_id'] = uids.sample
+	  	user_category['category_id'] = cids.sample
+	    UserCategory.create(user_category)
+	end
+end
+
+event_user = {}
+ActiveRecord::Base.transaction do
+	20.times do 
+	    event_user['user_id'] = uids.sample
+	  	event_user['event_id'] = eids.sample
+	    EventUser.create(event_user)
+	end
+end
