965d739037b3615f03b273a30843c4f065950849
nhnaoto <nhnaoto.nh@gmail.com>
2017-05-19 18:05:37 +0800

    able to use actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin_user', as: 'rails_admin'
+  mount ActionCable.server => '/cable'
+  resources :events, only: [:index] 
+  get "/events/push"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
