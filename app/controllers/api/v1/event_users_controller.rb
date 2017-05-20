db831445a28bf971a36891c295097f35ddff087b
teeyingyap <yapteeying@gmail.com>
2017-05-20 13:01:07 +0800

    able to show all the details for each event including host and guests

--- app/controllers/api/v1/event_users_controller.rb
+++ app/controllers/api/v1/event_users_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::EventUsersController < ApplicationController
+class Api::V1::EventUsersController < Api::V1::ApplicationController
   def create #this create method is called when someone joins an event
   	event_user_json = JSON.parse(request.body.read)
   	event_user_json.each do |x|
@@ -9,14 +9,11 @@ class Api::V1::EventUsersController < ApplicationController
   end 
 
   def index #this shows all the event that you've joined
-    # @my_joined_events = Event.where(id: EventUser.where(user_id: 1).each do |x|
-    # x.event_id
-    # end )
-    # render :json => @my_joined_events, :include => { :host => { :only => :fullname } }
-    @my_joined_events = EventUser.where(user_id: 1).all
-    render :json => @my_joined_events.to_json( :include => [{:event => { :include => {:host => { :only => :fullname }}}}])
+    @my_joined_events = EventUser.where(user_id: current_user.id).all
+    render :json => @my_joined_events.to_json( :include => [{:event => { :include => [{:guests => { :only => :fullname }  }, {:host => { :only => :fullname }} ]}}])
+    
     #render :json => @my_joined_events, :includes => [:events]
-
+#http://192.168.1.116:3000/api/v1/event_users?remember_token=a1376e38b8ca99f7e36d82d757b7ba1c109fdb0e
   end 
 
 end
