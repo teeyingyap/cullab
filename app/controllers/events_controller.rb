b26561b482f1fca5547f48616be3314faae72bf6
nhnaoto <nhnaoto.nh@gmail.com>
2017-05-22 19:37:51 +0800

    show avatar, subcategory's photo

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
 
   	# only for development environment
   	def push
-  		event = Event.new(user_id: User.first.id, category_id: Category.first.id, name: Faker::Food.ingredient, description: Faker::Lorem.sentence, event_time: Faker::Time.between(Date.today, 50.days.after))
+  		event = Event.new(user_id: User.first.id, category_id: Category.first.id, subcategory_id: 1, name: Faker::Food.ingredient, description: Faker::Lorem.sentence, event_time: Faker::Time.between(Date.today, 50.days.after))
   		event.save
   	end
 end
